3c2f9f2ff71710f07364bbd7ed14fb57367ad153
BegnazarAkh <begnazaraxmadjonov@mail.ru>
2023-09-02 18:51:56 +0200

    Stage-1, Generecis

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -22,7 +22,7 @@ public class Generics {
     //TODO: Refactor Method-3
     public <T> void cloneMethod(List<T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
-    }
+
 
 
 }
